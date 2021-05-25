e6618de5a549447cd1868f245f78c08d9c0cf938
陳鎮宇 <ccking37@gmail.com>
2021-05-25 22:00:00 +0800

    Finish the workshop.

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -6,7 +6,8 @@ import * as React from 'react'
 function MessagesDisplay({messages}) {
   const containerRef = React.useRef()
   // 🐨 replace useEffect with useLayoutEffect
-  React.useEffect(() => {
+  React.useLayoutEffect(() => {
+    console.log(containerRef.current.scrollHeight)
     containerRef.current.scrollTop = containerRef.current.scrollHeight
   })
 
@@ -33,7 +34,7 @@ function SlooooowSibling() {
   // how it impacts interactivity of the page before updates.
   React.useEffect(() => {
     // increase this number to see a more stark difference
-    sleep(300)
+    sleep(1000)
   })
   return null
 }
